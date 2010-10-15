dbd560dc4dd9b4eb28f5ade5c584db135b2d40ce
Caleb Anderson <caleb@l0ser.net>
2010-10-15 01:24:37 -0500

    motd failure avoidance

--- master/motd.php
+++ master/motd.php
@@ -1,19 +1,28 @@
 <?
-        $inVer = substr($_GET["v"], strpos($_GET["v"], "-")+1);
-        $verFile = "VERSION";
-        $handle = fopen($verFile, 'r');
+	$inVer = substr($_GET["v"], strpos($_GET["v"], "-")+1);
+	$verFile = "VERSION";
 
-        $version = fread($handle, filesize($verFile));
-        fclose($handle);
+		if (file_exists($verFile))
+				{
+							$handle = fopen($verFile, 'r');
 
-        $version = substr($version, strpos($version, "-")+1);
-        
-        if ($inVer < $version)
-        {
-                echo "New version available. Please go to http://open-ra.org to upgrade";
-        }
-        else
-        {
-                echo "Welcome to OpenRA. Read news and more at http://reddit.com/r/openra.";
-        }
+									$version = fread($handle, filesize($verFile));
+									fclose($handle);
+										}
+
+			$version = substr($version, strpos($version, "-")+1);
+
+			$output = "";
+
+				if ($inVer < $version)
+						{
+									$output = "New version " . $version . " available. Please go to http://open-ra.org to upgrade";
+										}
+					else
+							{
+										$output = "Welcome to OpenRA. Read news and more at http://reddit.com/r/openra.";
+											}
+				$output = strlen($output) . "|" . $output;
+
+				echo $output;
 ?>
